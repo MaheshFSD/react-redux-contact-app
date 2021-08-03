d5f4a1fe815199e6f66b07988b1196f7ec6404f6
MaheshFSD <maheshfsdeveloper@gmail.com>
2021-08-04 02:34:35 +0530

    Contact Manager app beginned

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+const Header = () => {
+    return ( 
+        <div >
+            <div className="ui fixed menu">
+            <div className="ui container center">
+                <h3>Contact Manager</h3>
+            </div>
+        </div>
+        </div>
+    )
+}
+
+export default Header
